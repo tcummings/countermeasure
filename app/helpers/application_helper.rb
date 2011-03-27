b76a40fa5afe551db669bccf18ba7a8d089142a2
tcummings <theocummings@gmail.com>
2011-03-26 17:03:06 -0700

    Remove default index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Title
+  def title
+    base_title = "Counter Measure"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
