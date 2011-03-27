0b2b121314bc288fcb287769ae0a8a34542a148a
tcummings <theocummings@gmail.com>
2011-03-26 19:53:34 -0700

    modified layout, routes and added basic user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Countermeasure::Application.routes.draw do
+
+  get "users/new"
+
+  root :to => 'pages#home'
+  
+  match '/register', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,10 +56,6 @@ Countermeasure::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
