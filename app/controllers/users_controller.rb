a064db2d391ac6fc8331d0346476dad2ab6f4ff4
tcummings <theocummings@gmail.com>
2011-04-10 20:17:48 -0700

    Basic user registration completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
     @title = "Register"
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Counter Measures!"
+      redirect_to @user
+    else
+      @title = "Register"
+      @user.password = ""
+      @user.password_confirmation = ""
+      render 'new'
+    end
   end
 
 end
