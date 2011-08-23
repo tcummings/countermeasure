a653c9033d352e6053a7e9511fcd5eb58817cdeb
tcummings <theocummings@gmail.com>
2011-08-22 23:17:11 -0700

    added carrierwave gem and uploading images

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.0.5'
 gem 'sqlite3-ruby', '1.3.3', :require => 'sqlite3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
 gem 'will_paginate', '3.0.pre2'
+gem 'rmagick'
+gem 'carrierwave'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,11 +1,12 @@
 class Bicycle < ActiveRecord::Base
-  attr_accessible :avatar
+  attr_accessible :avatar, :remote_avatar_url
   attr_accessible :serial_number
   attr_accessible :color
   attr_accessible :make
   attr_accessible :model
 
   belongs_to :user
+  mount_uploader :avatar, AvatarUploader
 
   validates :serial_number, :presence => true, :length => { :maximum => 100 }
   validates :model,         :presence => true, :length => { :maximum => 100 }
